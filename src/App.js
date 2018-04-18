3ced96d473381a0d299570f41d7bef371524a8d7
Logan <atmosphereasmr@gmail.com>
2018-04-17 22:42:40 -0700

    mario kart

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="bullet-bill"/>
+      <div className="big-hill"/>
+      <div className="flower"/>
+      <div className="mario"/>
+      <div className="shell"/>
+            <div className="green-horizon">
+
+</div>
+              <div className="bottom-line"/>
+        <div className="road">
+
+        </div>
+        <div className="bottom-line"/>
+      </div>
+    );
+  }
+}
+
+export default App;
